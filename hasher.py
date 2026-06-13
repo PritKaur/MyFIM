f992aff5a8e9f386093b47c482d61bb832d3563d
PritKaur <pritpal.kaur@strathmore.edu>
2026-06-13 16:10:35 +0300

    The SHA-256 hashing engine complete with comments

--- hasher.py
+++ hasher.py
@@ -1,19 +1,19 @@
-import hashlib
+import hashlib #Python's built-in hashing library
 
 #This function generates the hash for 1 file, reads that file in chunks and returns the hash as a hex string
-def generate_hash(file_path):
+def generate_hash(file_path): #The file path is for the file you want to hash and monitor
     sha256 = hashlib.sha256()  #This is the hash calculator
 
     #This block is saying: open file, feed into calculator in small chunks and return fingerprint    
-    with open(file_path,  'rb') as f: 
-        while chunk := f.read(8192):
+    with open(file_path,  'rb') as f: #Read-binary mode allows the monitoring of any file type, image, pdf, etc because python reads raw bytes
+        while chunk := f.read(8192): #Chunks prevent the tool from crashing when monitoring huge files 
             sha256.update(chunk)
 
-    return sha256.hexdigest()
+    return sha256.hexdigest() #the hash is returned as a readable 64-character hexadecimal string
 
 #TEST BLOCK - This will only run when I run this file directly, not when it's later imported by another script
 if __name__ == "__main__":
-    import os
+    import os #OS is only imported here because it's only needed for this testing part
 
     test_file = "test.txt"
 
