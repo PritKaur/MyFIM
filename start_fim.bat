36a95ce8c12a32bc1a57fbad269bca0f948919a8
PritKaur <pritpal.kaur@strathmore.edu>
2026-06-27 13:18:17 +0300

    Updated .bat file with mechanism for automatically closing terminal if user doesn't select a file or folder to monitor

--- start_fim.bat
+++ start_fim.bat
@@ -1,6 +1,13 @@
 @echo off
 echo Starting MyFIM...
+
 python baseline.py
+if %ERRORLEVEL% NEQ 0 (
+    echo Baseline creation was cancelled or failed. Closing MyFIM.
+    timeout /t 3 >nul
+    exit /b 1
+)
+
 start python dashboard.py
 python monitor.py
 pause
