306affb2a6ad210f393ef970fa1b07676e997d93
PritKaur <pritpal.kaur@strathmore.edu>
2026-07-02 21:08:48 +0300

    Updated so that dashboard can open first and terminals close properly if the user does not select a file or folder and clicks cancel instead

--- start_fim.bat
+++ start_fim.bat
@@ -1,13 +1,18 @@
 @echo off
 echo Starting MyFIM...
 
+start /min "MyFIM Dashboard" cmd /c python dashboard.py
+timeout /t 2 >nul
+
+start http://localhost:5000/login
+
 python baseline.py
 if %ERRORLEVEL% NEQ 0 (
     echo Baseline creation was cancelled or failed. Closing MyFIM.
+    taskkill /FI "WINDOWTITLE eq MyFIM Dashboard*" /T /F >nul 2>&1
     timeout /t 3 >nul
     exit /b 1
 )
 
-start python dashboard.py
-python monitor.py
+start python monitor.py
 pause
